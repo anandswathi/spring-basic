c89a8e429dd0845ee05f9374e68597dbfc0db90a
yaliprowess <yaliprowess@gmail.com>
2016-03-06 17:26:28 +0530

    Annotation based Bean Autowire

--- src/main/java/com/yaliprowess/spring/session/basic/message/impl/FileMessageReaderImpl.java
+++ src/main/java/com/yaliprowess/spring/session/basic/message/impl/FileMessageReaderImpl.java
@@ -1,33 +1,44 @@
 package com.yaliprowess.spring.session.basic.message.impl;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.yaliprowess.spring.session.basic.message.MessageReaderService;
 
 import org.apache.commons.io.IOUtils;
 
-import com.yaliprowess.spring.session.basic.message.MessageReaderService;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class FileMessageReaderImpl implements MessageReaderService{
-	private StringBuilder builder = null;
-	private InputStream in = null;
+public class FileMessageReaderImpl implements MessageReaderService {
 
-	public FileMessageReaderImpl(String fileName) {
-		ClassLoader classLoader = getClass().getClassLoader();
-		in = classLoader.getResourceAsStream(fileName);						
-	}
-	//Call back method -hooks
-	public void init(){
-		builder = new StringBuilder();
-	}
-	public void destroy(){		
-			builder = null;		
-	}
-	public String read() {		
-		try {
-			builder.append(IOUtils.toString(in));
-		} catch (IOException e) {
-			builder.append("Message recive error");
-		}		
-		return builder.toString();
-	}
+    private StringBuilder builder = null;
+    private String fileName = null;
+    private ClassLoader classLoader = null;
+
+    // Call back method -hooks
+    public void init() {
+        classLoader = getClass().getClassLoader();
+        builder = new StringBuilder();
+    }
+
+    public void destroy() {
+        builder = null;
+    }
+
+    public String read() {
+        try {
+            InputStream in = classLoader.getResourceAsStream(fileName);
+            builder.append(IOUtils.toString(in));
+        } catch (IOException e) {
+            builder.append("Message recive error");
+        }
+        return builder.toString();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
 }
--- src/main/java/com/yaliprowess/spring/session/basic/service/ReaderService.java
+++ src/main/java/com/yaliprowess/spring/session/basic/service/ReaderService.java
@@ -2,14 +2,25 @@ package com.yaliprowess.spring.session.basic.service;
 
 import com.yaliprowess.spring.session.basic.message.MessageReaderService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class ReaderService {
-	private MessageReaderService reader = null;
 
-	public ReaderService(MessageReaderService reader) {
-		this.reader = reader;
-	}
+    private MessageReaderService reader = null;
 
-	public String fetchData() {
-		return reader.read();
-	}
+    public MessageReaderService getReader() {
+        return reader;
+    }
+
+    @Autowired
+    // @Qualifier(value = "httpMessageReader")
+    @Qualifier(value = "fileMessageReader")
+    public void setReader(MessageReaderService reader) {
+        this.reader = reader;
+    }
+
+    public String fetchData() {
+        return reader.read();
+    }
 }
